e687489f4da84d0257da262c0609ac065647f070
kgoel085 <kgoel085@gmail.com>
2025-09-17 18:46:29 +0530

    added logs for GRPC

--- proto/client.go
+++ proto/client.go
@@ -17,9 +17,10 @@ func InitClients() {
 	ClientManager = NewManager()
 
 	if config.Config.GRPC.EmailServiceAddr != "" { // Email GRPC service
+		utils.Log.Info("GRPC:: connecting to email service at ", config.Config.GRPC.EmailServiceAddr)
 		_, err := ClientManager.Connect(string(EmailServiceClientType), config.Config.GRPC.EmailServiceAddr)
 		if err != nil {
-			utils.Log.Errorf("failed to connect to email service: %v", err)
+			utils.Log.Errorf("GRPC:: failed to connect to email service: %v", err)
 		} else {
 			utils.Log.Info("GRPC:: connected to email service")
 		}
